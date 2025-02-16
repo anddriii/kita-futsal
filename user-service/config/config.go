61f6d2d1e314e31e8fbdb2961974c49157905a58
Andrii <andristwan28@gmail.com>
2025-02-16 11:23:58 +0700

    implement config initialization

---
+++ user-service/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+
+	"github.com/anddriii/kita-futsal/user-service/common/util"
+	"github.com/sirupsen/logrus"
+)
+
+var Config AppConfig
+
+type AppConfig struct {
+	Port                  int      `json:"port"`
+	AppName               string   `json:"appName"`
+	AppEnv                string   `json:"appEnv"`
+	SignatureKey          string   `json:"signatureKey"`
+	Database              database `json:"database"`
+	RateLimitMaxRequest   float64  `json:"rateLimiterMaxRequest"`
+	RateLimiterTimeSecond int      `json:"rateLimiterTimeSecond"`
+	JwtScretKey           string   `json:"jwtSecretKey"`
+	JwtExpireTime         int      `json:"jwtExpirationTime"`
+}
+
+type database struct {
+	Host            string `json:"host"`
+	Port            int    `json:"port"`
+	Name            string `json:"name"`
+	UserName        string `json:"username"`
+	Password        string `json:"password"`
+	MaxOpenConn     int    `json:"maxOpenConnection"`
+	MaxLifeTimeConn int    `json:"maxLifetimeConnection"`
+	MaxIdleConn     int    `json:"maxIdleConnection"`
+	MaxIdleTime     int    `json:"maxIdleTime"`
+}
+
+/*
+jika config dari local maka akan mengambil dari file config.json.
+Tetapi jika confignya berasal dari grpc maka akan menggunakan util "BindFromConsul"
+*/
+func Init() {
+	err := util.BindFromJson(Config, "config.json", ".")
+	if err != nil {
+		logrus.Infof("Failed to bind config: %v", err)
+		err := util.BindFromConsul(Config, os.Getenv("CONSUL_HTTP_URL"), os.Getenv("CONSUL_HTTP_PATH"))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
