34f672c247fdf7031fe2cde02ffad95087e814ee
Andrii <andristwan28@gmail.com>
2025-02-27 16:27:23 +0700

    feat: add DTO definitions for field scheduling and time management

---
+++ field-service/domains/dto/field_schedule.go
@@ -0,0 +1,57 @@
+package dto
+
+import (
+	"time"
+
+	"github.com/anddriii/kita-futsal/field-service/constants"
+	"github.com/google/uuid"
+)
+
+type FieldScheduleRequest struct {
+	FieldID string   `json:"fieldID" validate:"required"`
+	Date    string   `json:"date" validate:"required"`
+	TimeIDs []string `json:"timeIDs" validate:"required"`
+}
+
+type GenerateFieldScheduleForOneMonthRequest struct {
+	FieldID string `json:"fieldID" validate:"required"`
+}
+
+type UpdateScheduleRequest struct {
+	Date    string `json:"date" validate:"required"`
+	TimeIDs string `json:"timeID" validate:"required"`
+}
+
+type UpdateStatusFieldScheduleRequest struct {
+	FieldScheduleIDs []string `json:"fieldScheduleIDs" validate:"required"`
+}
+
+type FieldScheduleResponse struct {
+	UUID         uuid.UUID                     `json:"uuid"`
+	FieldName    string                        `json:"fieldName"`
+	PricePerHour int                           `json:"pricePerHour"`
+	Date         string                        `json:"date"`
+	Status       constants.FieldScheduleStatus `json:"status"`
+	Time         string                        `json:"time"`
+	CreatedAt    *time.Time
+	UpdateAt     *time.Time
+}
+
+type FieldScheduleForBookingReponse struct {
+	UUID         uuid.UUID                     `json:"uuid"`
+	PricePerHour int                           `json:"pricePerHour"`
+	Date         string                        `json:"date"`
+	Status       constants.FieldScheduleStatus `json:"status"`
+	Time         string                        `json:"time"`
+}
+
+type FieldScheduleRequestParam struct {
+	Page       int     `form:"page" validate:"required"`
+	Limit      int     `form:"page" validate:"required"`
+	SortColumn *string `form:"sortColumn"`
+	SortOrder  *string `form:"sortOrder"`
+}
+
+type FieldScheduleByFieldIDAndDateRequestParam struct {
+	Date string `json:"date" validate:"required"`
+}
