0147667366fc5f78d638e459560c951e00e00071
Andrii <andristwan28@gmail.com>
2025-03-01 08:39:49 +0700

    feat: add pagination utility and SHA-256 hash generation functions

--- field-service/common/util/util.go
+++ field-service/common/util/util.go
@@ -1,17 +1,91 @@
 package util
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"reflect"
 	"strconv"
 	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	_ "github.com/spf13/viper/remote"
 )
+
+// PaginationParam merepresentasikan parameter untuk paginasi.
+type PaginationParam struct {
+	Count int64       `json:"count"` // Total jumlah data
+	Page  int         `json:"page"`  // Halaman saat ini
+	Limit int         `json:"limit"` // Jumlah item per halaman
+	Data  interface{} `json:"data"`  // Data yang akan dipaginasi
+}
+
+// PaginationResult merepresentasikan hasil dari proses paginasi.
+type PaginationResult struct {
+	TotalPage    int         `json:"totalPage"`    // Total jumlah halaman
+	TotalData    int64       `json:"totalData"`    // Total jumlah data
+	NextPage     *int        `json:"nextPage"`     // Halaman berikutnya (jika ada)
+	PreviousPage *int        `json:"previousPage"` // Halaman sebelumnya (jika ada)
+	Page         int         `json:"page"`         // Halaman saat ini
+	Limit        int         `json:"limit"`        // Jumlah item per halaman
+	Data         interface{} `json:"data"`         // Data yang dipaginasi
+}
+
+// GeneratePagination menghasilkan hasil paginasi berdasarkan parameter yang diberikan.
+func GeneratePagination(params PaginationParam) PaginationResult {
+	totalPage := int(math.Ceil(float64(params.Count) / float64(params.Limit)))
+
+	var (
+		nextPage     int
+		previousPage int
+	)
+
+	// Menentukan halaman berikutnya jika masih ada
+	if params.Page < totalPage {
+		nextPage = params.Page + 1
+	}
+
+	// Menentukan halaman sebelumnya jika lebih dari 1
+	if params.Page > 1 {
+		previousPage = params.Page - 1
+	}
+
+	return PaginationResult{
+		TotalPage:    totalPage,
+		TotalData:    params.Count,
+		NextPage:     &nextPage,
+		PreviousPage: &previousPage,
+		Page:         params.Page,
+		Limit:        params.Limit,
+		Data:         params.Data,
+	}
+}
+
+// GenerateSHA256 menghasilkan hash SHA-256 dari string input.
+func GenerateSHA256(inputString string) string {
+	hash := sha256.New()
+	hash.Write([]byte(inputString))
+	hashBytes := hash.Sum(nil)
+
+	// Mengembalikan hasil hash dalam format string hexadecimal
+	return hex.EncodeToString(hashBytes)
+}
+
+// RupiahFormat mengonversi angka menjadi format mata uang Rupiah.
+func RupiahFormat(amount *float64) string {
+	stringValue := "0"
+	if amount != nil {
+		// Menggunakan humanize untuk menambahkan separator ribuan
+		humanizeValue := humanize.CommafWithDigits(*amount, 0)
+		stringValue = strings.ReplaceAll(humanizeValue, ",", ".")
+	}
+	return fmt.Sprintf("Rp. %s", stringValue)
+}
 
 // digunakan untuk membaca file JSON dari server cloud atau dari Consul (penyimpanan konfigurasi berbasis key-value).
 
