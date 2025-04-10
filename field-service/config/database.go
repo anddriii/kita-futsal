847b3b343ee83af75ba4f9cc192fa1f3594688ba
Andrii <andristwan28@gmail.com>
2025-04-10 21:31:58 +0700

    chore(database): switch database driver to PostgreSQL and update connection logic

--- field-service/config/database.go
+++ field-service/config/database.go
@@ -6,40 +6,20 @@ import (
 	"net/url"
 	"time"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func InitDB() (*gorm.DB, error) {
 	config := Config
 	encodedPassword := url.QueryEscape(config.Database.Password)
-	// uri := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", config.Database.UserName, encodedPassword, config.Database.Host, config.Database.Port, config.Database.Name)
+	uri := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", config.Database.UserName, encodedPassword, config.Database.Host, config.Database.Port, config.Database.Name)
 
-	// fmt.Printf("Database Config: %+v\n", config.Database)
+	fmt.Printf("Database Config: %+v\n", config.Database)
 
-	// db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// sqlDB, err := db.DB()
-	// sqlDB.SetMaxOpenConns(config.Database.MaxOpenConn)
-	// sqlDB.SetConnMaxLifetime(time.Duration(config.Database.MaxLifeTimeConn) * time.Second)
-	// sqlDB.SetMaxIdleConns(config.Database.MaxIdleConn)
-	// sqlDB.SetConnMaxIdleTime(time.Duration(config.Database.MaxIdleTime) * time.Second)
-
-	// ==================================FOR DB MYSQL===================================
-	//for local
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Database.UserName, encodedPassword, config.Database.Host, config.Database.Port, config.Database.Name)
-
-	// Open connection
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info), // Enable SQL logging
-	})
-
+	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("failed to connect database: %v", err)
+		return nil, err
 	}
 
 	sqlDB, err := db.DB()
@@ -49,6 +29,28 @@ func InitDB() (*gorm.DB, error) {
 	sqlDB.SetMaxOpenConns(config.Database.MaxOpenConn)
 	sqlDB.SetConnMaxLifetime(time.Duration(config.Database.MaxLifeTimeConn) * time.Second)
 	sqlDB.SetMaxIdleConns(config.Database.MaxIdleConn)
+	sqlDB.SetConnMaxIdleTime(time.Duration(config.Database.MaxIdleTime) * time.Second)
+
+	// // ==================================FOR DB MYSQL===================================
+	// //for local
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Database.UserName, encodedPassword, config.Database.Host, config.Database.Port, config.Database.Name)
+
+	// // Open connection
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	// 	Logger: logger.Default.LogMode(logger.Info), // Enable SQL logging
+	// })
+
+	// if err != nil {
+	// 	log.Fatalf("failed to connect database: %v", err)
+	// }
+
+	// sqlDB, err := db.DB()
+	// if err != nil {
+	// 	log.Fatalf("failed to get database instance: %v", err)
+	// }
+	// sqlDB.SetMaxOpenConns(config.Database.MaxOpenConn)
+	// sqlDB.SetConnMaxLifetime(time.Duration(config.Database.MaxLifeTimeConn) * time.Second)
+	// sqlDB.SetMaxIdleConns(config.Database.MaxIdleConn)
 
 	return db, nil
 
