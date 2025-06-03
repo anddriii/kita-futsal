0d99612e1d047f00a89ef0c0c46dc5f7fc6d2391
Andri <andristwan28@gmail.com>
2025-06-03 10:27:41 +0700

    feat(payment-service): Add Payment and PaymentHistory models with GORM annotations

---
+++ payment-service/domains/models/payment_history.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"github.com/anddriii/kita-futsal/payment-service/constants"
+)
+
+type PaymentHistory struct {
+	ID        uint                          `gorm:"primaryKey;autoIncrement"`
+	PaymentID uint                          `gorm:"type:bigint;not null"`
+	Status    constants.PaymentStatusString `gorm:"type:varchar(50);not null"`
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+}
