8a11d9d91b20171922eba3c9d0b5364a628b149a
Andriii <andristwan28@gmail.com>
2026-01-21 16:18:55 +0700

    feat(order-service): implement PaymentClient with CreatePaymentLink and GetPaymentByUUID methods, and add PaymentRequest and PaymentData DTOs

--- order-service/clients/payment/payment.go
+++ order-service/clients/payment/payment.go
@@ -1,1 +1,32 @@
 package clients
+
+import (
+	"context"
+
+	"github.com/anddriii/kita-futsal/order-service/clients/config"
+	"github.com/anddriii/kita-futsal/order-service/domain/dto"
+	"github.com/google/uuid"
+)
+
+type PaymentClient struct {
+	client config.IClientConfig
+}
+
+// CreatePaymentLink implements IPaymentClient.
+func (p *PaymentClient) CreatePaymentLink(context.Context, *dto.PaymentRequest) (*PaymentData, error) {
+	panic("unimplemented")
+}
+
+// GetPaymentByUUID implements IPaymentClient.
+func (p *PaymentClient) GetPaymentByUUID(context.Context, uuid.UUID) (*PaymentData, error) {
+	panic("unimplemented")
+}
+
+type IPaymentClient interface {
+	GetPaymentByUUID(context.Context, uuid.UUID) (*PaymentData, error)
+	CreatePaymentLink(context.Context, *dto.PaymentRequest) (*PaymentData, error)
+}
+
+func NewPaymentClient(client config.IClientConfig) IPaymentClient {
+	return &PaymentClient{client: client}
+}
