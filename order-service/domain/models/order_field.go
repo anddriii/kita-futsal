9bb76aaf2229c13a060f20618f03e3b270aa75f8
Andrii <andristwan28@gmail.com>
2026-04-28 20:49:20 +0700

    fix(order-service): correct typo in OrderField struct field name from FieldSCheduleID to FieldScheduleID

--- order-service/domain/models/order_field.go
+++ order-service/domain/models/order_field.go
@@ -9,11 +9,7 @@ import (
 type OrderField struct {
 	ID              uint      `gorm:"primaryKey;autoIncrement"`
 	OrderID         uint      `gorm:"type:bigint;not null"`
-<<<<<<< HEAD
 	FieldSCheduleID uuid.UUID `gorm:"type:uuid;not null"`
-=======
-	FieldScheduleID uuid.UUID `gorm:"type:uuid;not null"`
->>>>>>> ad31e98 (feat(order-service): update KafkaMessage struct fields and enhance OrderRequest validation; add middleware for panic handling and rate limiting)
 	CreatedAt       *time.Time
 	UpdatedAt       *time.Time
 }
